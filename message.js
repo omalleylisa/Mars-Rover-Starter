e91680d1d9d4769b4069115baab620b8b93d2887
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-02-28 11:38:45 -0600

    graded assignment

--- message.js
+++ message.js
@@ -1,7 +1,7 @@
 class Message {
    constructor(name, commands = ['MOVE', 'STATUS_CHECK', 'MODE_CHANGE']) {
      if (!name) {
-       throw new Error('Name required. ');
+       throw new Error('Name required.');
      }
      this.name = name;
      this.commands = commands;
